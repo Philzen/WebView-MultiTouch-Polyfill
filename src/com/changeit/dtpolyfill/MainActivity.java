b094f300f026bffdea8fd41313823dd7d21d84e9
Phil. Austermann <phil@patient-radio.de>
2012-05-25 02:47:38 +0200

    # Refactoring (removed detour via layout.xml)

--- src/com/changeit/dtpolyfill/MainActivity.java
+++ src/com/changeit/dtpolyfill/MainActivity.java
@@ -12,11 +12,12 @@ public class MainActivity extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
 
-		WebView webview = (WebView) this.findViewById(R.id.webView);
+		WebView webview = new WebView(this);
 		webview.setWebViewClient(new WebClient());
 		webview.getSettings().setJavaScriptEnabled(true);
 		webview.loadUrl("file:///android_asset/www/index.html");
+		
+		setContentView(webview);
     }
 }
