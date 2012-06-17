9e11841c8e638ecf33a70ec08bdfec7e25dfe64b
Phil. Austermann <phil@patient-radio.de>
2012-06-17 21:55:33 +0200

    + added check if move has been at least one pixel wide

--- src/com/changeit/wmpolyfill/WebClient.java
+++ src/com/changeit/wmpolyfill/WebClient.java
@@ -27,6 +27,8 @@ public class WebClient extends WebViewClient {
 	/** The desired number of touches you'd need in your application (-1 = ALL) */
 	protected int maxTouches = -1;
 
+	private MotionEvent lastMotionEvent = null;
+
 	@Override
 	public boolean shouldOverrideUrlLoading(WebView view, String url) {
 		view.loadUrl(url);
@@ -52,9 +54,13 @@ public class WebClient extends WebViewClient {
 					view.loadUrl("javascript: decrementTapCount();");
 				}
 
-				if (arg1.getAction() != MotionEvent.ACTION_MOVE) {
-					view.loadUrl("javascript: debug('" + dumpEvent(arg1) + "');");
-				}
+				String EventJSON = new String(getEvent(arg1));
+				if (EventJSON.length() > 0)
+//				if (arg1.getAction() != MotionEvent.ACTION_MOVE) {
+					view.loadUrl("javascript: debug('" + getEvent(arg1) + "');");
+//				}
+
+				lastMotionEvent = MotionEvent.obtain(arg1);
 
 				if (polyfillAlltouches || (maxTouches > 0 && arg1.getPointerCount() > maxNativeTouches) ) {
 					return true;
@@ -67,6 +73,88 @@ public class WebClient extends WebViewClient {
 				return false;
 			}
 		});
+	}
+
+	/**
+	 * Function to check if coordinates between two moves have changed by at least one pixel
+	 * @return
+	 */
+	private boolean hasMoved(MotionEvent event)	{
+
+		/**
+		 * In almost all cases the moving finger triggered the motion event
+		 * therefore there is no need to loop through all Pointer indexes here (which would be the thorough way)
+		 */
+		if ( lastMotionEvent == null
+			||  (int)lastMotionEvent.getX() != (int)event.getX()
+			|| (int)lastMotionEvent.getY() != (int)event.getY() )
+			return true;
+
+		return false;
+	}
+
+	/**
+	 * Return a JSON representation of the pointer information that is intented to be sent to the javascript runtime
+	 * @param event A motion Event
+	 * @return
+	 */
+	private String getMoveJSON(MotionEvent event) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < event.getPointerCount(); i++) {
+			sb.append("{").append(event.getPointerId(i))
+					.append(": [")
+					.append((int)event.getX(i)).append(", ")
+					.append((int) event.getY(i)).append(", ")
+					.append("]")
+					.append("}");
+			if (i + 1 < event.getPointerCount()) {
+				sb.append(",");
+			}
+		}
+		return sb.toString();
+	}
+
+	private String getEvent(MotionEvent event) {
+		String names[] = {"DOWN", "UP", "MOVE", "CANCEL", "OUTSIDE",
+			"POINTER_DOWN", "POINTER_UP", "7?", "8?", "9?"};
+
+		StringBuilder sb = new StringBuilder();
+		int action = event.getAction();
+		int actionCode = action & MotionEvent.ACTION_MASK;
+		//sb.append("code").append( actionCode );
+		if (actionCode == MotionEvent.ACTION_MOVE) {
+			/* Tracking each and every move would be total javascript runtime overkill,
+			 * therefore only changes by at least one pixel will be tracked
+			 */
+			if (hasMoved(event)) {
+				sb.append("\"move\" ").append(getMoveJSON(event));
+				// At least one touch event has changed
+			}
+		} else if (actionCode == MotionEvent.ACTION_POINTER_DOWN
+			|| actionCode == MotionEvent.ACTION_DOWN) {
+			sb.append("\"down\", ").append(event.getPointerId(event.getActionIndex()));
+		} else if (actionCode == MotionEvent.ACTION_POINTER_UP
+			|| actionCode == MotionEvent.ACTION_UP) {
+			sb.append("\"up\", ").append(event.getPointerId(event.getActionIndex()));
+		} else if (actionCode == MotionEvent.ACTION_CANCEL) {
+			sb.append("\"cancel\", ").append(event.getPointerId(event.getActionIndex()));
+		}
+
+
+//		sb.append("ACTION_").append(names[actionCode]);
+//		sb.append(" [");
+//		for (int i = 0; i < event.getPointerCount(); i++) {
+//			sb.append("#").append(i);
+//			sb.append("(pid_").append(event.getPointerId(i));
+//			sb.append(")=").append((int) event.getX(i));
+//			sb.append(",").append((int) event.getY(i));
+//			if (i + 1 < event.getPointerCount()) {
+//				sb.append("; ");
+//			}
+//		}
+//		sb.append("]");
+		return sb.toString();
+
 	}
 
 	/**
