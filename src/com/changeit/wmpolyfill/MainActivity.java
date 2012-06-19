8b92476ea2913407a15728c717fe83d8d9629d4e
Phil. Austermann <phil@patient-radio.de>
2012-06-20 00:04:55 +0200

    + Added progress bar (loading of external pages can take long, so better provide some feedback)

--- src/com/changeit/wmpolyfill/MainActivity.java
+++ src/com/changeit/wmpolyfill/MainActivity.java
@@ -1,14 +1,17 @@
 package com.changeit.wmpolyfill;
 
 import android.app.Activity;
-import android.webkit.WebView;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
 
 public class MainActivity extends Activity
 {
-    /** Called when the activity is first created. */
+	/** Called when the activity is first created.
+	 * @param savedInstanceState
+	 */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
@@ -21,14 +24,28 @@ public class MainActivity extends Activity
 		webview.setHorizontalScrollBarEnabled(false);
 		webview.loadUrl("file:///android_asset/www/index.html");
 
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		// Hide the status bar at the top
+		this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		// Adds Progress bar Support
+		this.getWindow().requestFeature(Window.FEATURE_PROGRESS);
+		// Makes Progress bar Visible
+		getWindow().setFeatureInt( Window.FEATURE_PROGRESS, Window.PROGRESS_VISIBILITY_ON);
 
+		final Activity MyActivity = this;
+		webview.setWebChromeClient(new WebChromeClient() {
+			@Override
+			public void onProgressChanged(WebView view, int progress)
+			{
+				//Make the bar disappear after URL is loaded, and changes string to Loading...
+				MyActivity.setTitle("Loading...");
+				MyActivity.setProgress(progress * 100); //Make the bar disappear after URL is loaded
 
-/**
- * Possible test pages to call instead of assets/www/index.html:
- * http://inserthtml.com/demo/mobile-touch/
- */
+				// Return the app name after finish loading
+				if(progress == 100) {
+					MyActivity.setTitle(R.string.app_name);
+				}
+			}
+		});
 
 		setContentView(webview);
     }
