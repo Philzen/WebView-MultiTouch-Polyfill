1d87a527c72a7d7546b25229f8894ec9ac850e23
fastr <strachanski@googlemail.com>
2012-07-03 02:17:28 +0200

    update comments

--- src/com/changeit/wmpolyfill/WebClientTouchUpdater.java
+++ src/com/changeit/wmpolyfill/WebClientTouchUpdater.java
@@ -4,7 +4,6 @@ package com.changeit.wmpolyfill;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.TimerTask;
-
 import android.util.Log;
 import android.webkit.WebView;
 
@@ -17,6 +16,9 @@ public class WebClientTouchUpdater extends TimerTask{
 	private WebView view;
 	private WebClient wmp;
 	@Override
+	/**
+	 * Firing the touches collected between the last time and now
+	 */
 	public void run() {
 		ArrayList<String> touches = wmp.getTouches();
 		Iterator<String> itr = touches.iterator();
@@ -25,9 +27,12 @@ public class WebClientTouchUpdater extends TimerTask{
 	    	//TODO: hand over the complete array in _one_ javascript call...
 	    	view.loadUrl("javascript: WMP.polyfill(" + itr.next() + ");");
 	    }
-		// TODO Auto-generated method stub
-		
 	}
+	/**
+	 * Constructor
+	 * @param wmp
+	 * @param view
+	 */
 	public WebClientTouchUpdater(WebClient wmp, WebView view){
 		super();
 		this.wmp = wmp;	
