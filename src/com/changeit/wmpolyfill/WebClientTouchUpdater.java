7135375830d35771d953f5638f51da3ff2bb55df
fastr <strachanski@googlemail.com>
2012-07-03 02:08:25 +0200

    Added WebClientTouchUpdater A TimerTask class to separate input of touches in java and triggering touches in javascript.

---
+++ src/com/changeit/wmpolyfill/WebClientTouchUpdater.java
@@ -0,0 +1,36 @@
+
+package com.changeit.wmpolyfill;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.TimerTask;
+
+import android.util.Log;
+import android.webkit.WebView;
+
+/**
+ * Class implements a TimerTask that sends the touches collected by WebClient to the WebApp
+ * @author fastr
+ *
+ */
+public class WebClientTouchUpdater extends TimerTask{
+	private WebView view;
+	private WebClient wmp;
+	@Override
+	public void run() {
+		ArrayList<String> touches = wmp.getTouches();
+		Iterator<String> itr = touches.iterator();
+	    while (itr.hasNext()) {
+	    	Log.d("wmp.console","Sending Polyfills");
+	    	//TODO: hand over the complete array in _one_ javascript call...
+	    	view.loadUrl("javascript: WMP.polyfill(" + itr.next() + ");");
+	    }
+		// TODO Auto-generated method stub
+		
+	}
+	public WebClientTouchUpdater(WebClient wmp, WebView view){
+		super();
+		this.wmp = wmp;	
+		this.view = view;
+	}
+}
