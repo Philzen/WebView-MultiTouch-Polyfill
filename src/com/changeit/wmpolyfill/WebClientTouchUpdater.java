16c4243d6a9f298969f1e039b2659bdda438a32f
Phil. Austermann <phil@patient-radio.de>
2013-02-02 19:27:36 +0100

    # neue Bedingung für Ausführung des Batches # reformatted

--- src/com/changeit/wmpolyfill/WebClientTouchUpdater.java
+++ src/com/changeit/wmpolyfill/WebClientTouchUpdater.java
@@ -8,33 +8,45 @@ import android.webkit.WebView;
 
 /**
  * Class implements a TimerTask that sends the touches collected by WebClient to the WebApp
+ *
  * @author fastrde
  *
  */
-public class WebClientTouchUpdater extends TimerTask{
-	private WebView view;
-	private WebClient wmp;
-	@Override
-	/**
-	 * Firing the touches collected between the last time and now
-	 */
-	public void run() {
-		ArrayList<String> touches = wmp.getTouches();
-		Iterator<String> itr = touches.iterator();
+public class WebClientTouchUpdater extends TimerTask
+{
+
+    private WebView view;
+    private WebClient wmp;
+
+    @Override
+    /**
+     * Firing the touches collected between the last time and now
+     */
+    public void run()
+    {
+	ArrayList<String> touches = wmp.getTouches();
+	if (touches.size() > 0) {
+	    Iterator<String> itr = touches.iterator();
+	    Log.d("changeit.wmp", "Sending Batch: ");
 	    while (itr.hasNext()) {
-	    	Log.d("wmp.console","Sending Polyfills");
-	    	//TODO: hand over the complete array in _one_ javascript call...
-	    	view.loadUrl("javascript: WMP.polyfill(" + itr.next() + ");");
+		String wmpTouch = itr.next();
+		Log.d("changeit.wmp", "Sending " + wmpTouch);
+		//TODO: hand over the complete array in _one_ javascript call...
+		view.loadUrl("javascript: WMP.polyfill(" + wmpTouch + ");");
 	    }
 	}
-	/**
-	 * Constructor
-	 * @param wmp
-	 * @param view
-	 */
-	public WebClientTouchUpdater(WebClient wmp, WebView view){
-		super();
-		this.wmp = wmp;	
-		this.view = view;
-	}
+    }
+
+    /**
+     * Constructor
+     *
+     * @param wmp
+     * @param view
+     */
+    public WebClientTouchUpdater(WebClient wmp, WebView view)
+    {
+	super();
+	this.wmp = wmp;
+	this.view = view;
+    }
 }
