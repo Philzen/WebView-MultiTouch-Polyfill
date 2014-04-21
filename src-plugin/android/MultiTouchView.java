065e692df9f2f97827a6957bec52a17b26907228
Philzen <phil@patient-radio.de>
2014-04-21 18:31:16 +0200

    + basic plugin

---
+++ src-plugin/android/MultiTouchView.java
@@ -0,0 +1,28 @@
+
+package org.changeit.android2.multitouch;
+
+import com.changeit.wmpolyfill.*;
+import org.apache.cordova.CordovaInterface;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CordovaWebView;
+
+/*
+ * @author philzen
+ */
+public class MultiTouchView extends CordovaPlugin {
+
+    private CordovaWebClient wmp;
+
+    /**
+     * Sets the context of the Command. This can then be used to do things like
+     * get file paths associated with the Activity.
+     *
+     * @param cordova The context of the main Activity.
+     * @param webView The CordovaWebView Cordova is running in.
+     */
+    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+        super.initialize(cordova, webView);
+
+        wmp = new CordovaWebClient(cordova, webView);
+    }
+}
